ecdabe6895e3c47b400d93d299279f5f67c7c8e2
kennytrionfo <kentrionfo@gmail.com>
2014-08-25 17:55:11 -0600

    putting in views and pics

--- config/routes.rb
+++ config/routes.rb
@@ -1,6 +1,14 @@
 Rails.application.routes.draw do
-  # The priority is based upon order of creation: first created -> highest priority.
-  # See how all your routes lay out with "rake routes".
+
+
+  
+  get 'site/index'
+
+  get 'site/show'
+
+  get 'site/new'
+
+  get 'site/create'
 
   # You can have the root of your site routed with "root"
   # root 'welcome#index'
